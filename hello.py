db1af18d6698a028a733e582ec14688bdc68a074
circus2271 <rodionov134@gmail.com>
2023-12-16 14:55:51 +0300

    update

--- hello.py
+++ hello.py
@@ -10,6 +10,13 @@ f = f'hello {name}'
 
 print('hello from python script')
 
+pages = ['about', 'homepage', 'contacts']
+
+nav = ''
+for page in pages:
+	a = f'<a href="/{page}">{page}</a>'
+	nav += a
+
 
 class ExtendedHTTPRequestHandler(BaseHTTPRequestHandler):
 
@@ -17,17 +24,37 @@ class ExtendedHTTPRequestHandler(BaseHTTPRequestHandler):
 #		print('before logging request')
 		#print(self.headers)
 		print('path:', self.path)
+		
+		if self.path.endswith('.css'):
+			self.send_response(200)
+			self.send_header('Content-type', 'text/css')
+			self.end_headers()
 
-		if self.path == '/':
-			content = 'homepage'
-		else:
-			content = 'elsewhere'
+			with open('style.css', 'r', encoding='utf-8') as styles:
+				self.wfile.write(styles.read().encode('utf-8'))
+				return
+
+		#if self.path == '/':
+		#	content = 'homepage'
+		#else:
+		#	content = 'elsewhere'
 #		print('after logging request')
 		self.send_response(200)
-		self.send_header('Content-type','text/plain')
+		self.send_header('Content-type','text/html')#'text/plain')
 		self.end_headers()
 
-		self.wfile.write(f'{f} {message} {content}'.encode('utf-8'))
+
+
+		#script = '<script>alert(\'wow\')</script>'
+		#template = Template('2$script')
+		#a = template.substitute(script=script)
+
+		self.wfile.write('<link rel="stylesheet" href="style.css">'.encode('utf-8'))
+		self.wfile.write(nav.encode('utf-8'))
+		self.wfile.write('wow'.encode('utf-8'))
+		#self.wfile.write(a.encode('utf-8'))
+		#self.wfile.write(script.encode('utf-8'))
+#		self.wfile.write(f'{f} {message} {content} {script}'.encode('utf-8'))
 #		self.wfile.write('Hello, lol1world! from $content'.encode('utf-8'))
 		return
 
