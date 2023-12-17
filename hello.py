23a6eb070af2c30720903a266f6434e83a63887a
circus2271 <rodionov134@gmail.com>
2023-12-17 16:00:25 +0300

    onother update

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
 from string import Template
 
 
@@ -10,52 +10,41 @@ f = f'hello {name}'
 
 print('hello from python script')
 
-pages = ['about', 'homepage', 'contacts']
-
-nav = ''
-for page in pages:
-	a = f'<a href="/{page}">{page}</a>'
-	nav += a
+pages = ['', 'about', 'homepage', 'contacts']
 
 
-class ExtendedHTTPRequestHandler(BaseHTTPRequestHandler):
+def nav(current_url):
+	html = '<nav><ul>'
+	for page in pages:
+		is_active = 'active' if page == current_url else ''
+		li = f'<li><a href="/{page}" class="{is_active}">{page}</a></li>'
+		html += li
+	html += '</ul></nav>'
+	return html
+
+
+class ExtendedHTTPRequestHandler(SimpleHTTPRequestHandler):
 
 	def do_GET(self):
-#		print('before logging request')
-		#print(self.headers)
 		print('path:', self.path)
+		current_url = self.path[1:]
+		# if True:
 		
-		if self.path.endswith('.css'):
-			self.send_response(200)
-			self.send_header('Content-type', 'text/css')
-			self.end_headers()
+		if current_url not in pages:
 
-			with open('style.css', 'r', encoding='utf-8') as styles:
-				self.wfile.write(styles.read().encode('utf-8'))
-				return
+		#if self.path.endswith('.css'):
+			super().do_GET()
+			return
 
-		#if self.path == '/':
-		#	content = 'homepage'
-		#else:
-		#	content = 'elsewhere'
-#		print('after logging request')
+
+
 		self.send_response(200)
 		self.send_header('Content-type','text/html')#'text/plain')
 		self.end_headers()
 
-
-
-		#script = '<script>alert(\'wow\')</script>'
-		#template = Template('2$script')
-		#a = template.substitute(script=script)
-
 		self.wfile.write('<link rel="stylesheet" href="style.css">'.encode('utf-8'))
-		self.wfile.write(nav.encode('utf-8'))
+		self.wfile.write(nav(current_url).encode('utf-8'))
 		self.wfile.write('wow'.encode('utf-8'))
-		#self.wfile.write(a.encode('utf-8'))
-		#self.wfile.write(script.encode('utf-8'))
-#		self.wfile.write(f'{f} {message} {content} {script}'.encode('utf-8'))
-#		self.wfile.write('Hello, lol1world! from $content'.encode('utf-8'))
 		return
 
 def run(server_class=HTTPServer, handler_class=ExtendedHTTPRequestHandler):
