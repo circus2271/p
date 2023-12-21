6032a1f6beafc28c432ff108658f6955c3e358ac
circus2271 <rodionov134@gmail.com>
2023-12-21 17:12:40 +0300

    update

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
 from string import Template
+from datetime import date
 
 from jinja2 import Environment, PackageLoader, select_autoescape, TemplateNotFound
 
@@ -28,8 +29,7 @@ navlinks = [
     }
 ]
 
-available_pages = [link['url'] for link in navlinks]
-#print(template.render(navlinks=navlinks, current_page='contacts'))
+predefined_links = [link['url'] for link in navlinks]
 
 t = Template('Hello, $name')
 message = t.substitute(name='Ivan')
@@ -39,8 +39,6 @@ f = f'hello {name}'
 
 print('hello from python script')
 
-
-# pages = ['homepage', 'about', 'contacts', 'no_template']
 
 events = [
     {
@@ -62,77 +60,72 @@ class ExtendedHTTPRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         print('path:', self.path)
         current_url = self.path
-        # if True:
 
-        # if current_url not in pages:
-        #if current_url.startswith('/assets'):
-        if self.path.startswith('/assets'):
+        if current_url.startswith('/assets'):
             super().do_GET()
             return
 
-        #if current_url == '':
-        #    curent_page = 'homepage'
-        #else:
-        #    curent_page = current_url
+        if current_url in predefined_links:
+            if current_url == '/':
+                template_name = 'index.html'
+            else:
+                template_name = f'{current_url[1:]}.html'
 
-        if current_url == '/':
-            template_name = 'index'
-        else:
-            template_name = current_url[1:] # remove slash from url start
+            try:
+                template = env.get_template(template_name)
+            except TemplateNotFound:
+                self.send_header('Content-type', 'text/html')
+                self.send_error(404, message="Template not found")
+                return
 
+            context = {
+                'navlinks': navlinks,
+                'current_url': current_url,
+                'current_year': date.today().strftime('%Y')
+            }
 
-        if current_url not in available_pages:
-            cur_enent = None
+            if current_url == '/':
+                context['events'] = events
+
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+
+            self.wfile.write(template.render(context=context).encode('utf-8'))
+            return
+
+        if current_url not in predefined_links:
+            # maybe it's an event detail page
+            # find event by its id
+            # if matches, show its detail page
+            current_event = None
             for event in events:
-                print('templaten', template_name)
                 print('event id', str(event['id']))
-                # print(template_name == str(event['id']))
-                # if template_name == str(event['id']):
-                #     break
                 if current_url[1:] == str(event['id']):
-                    # template = env.get_template('event.html')
-                    cur_enent = event
+                    current_event = event
                     break
 
-            if cur_enent:
-                print('curenene', cur_enent)
+            if current_event:
+                print('curenene', current_event)
                 # print(template_name == str(event['id']))
-                self.send_header('Content-type', 'text/html')
                 self.send_response(200)
+                self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
                 template = env.get_template('event.html')
-                print(template.render(event=cur_enent))
-                self.wfile.write(template.render(navlinks=navlinks, current_url=current_url, event=events[0]).encode('utf-8'))
+                context = {
+                    'navlinks': navlinks,
+                    'current_url': current_url,
+                    'event': events[current_event['id'] - 1],
+                    'current_year': date.today().strftime('%Y')
+                }
+                self.wfile.write(template.render(context=context).encode('utf-8'))
 
-             #   self.wfile.write(template.render(event=cur_enent, navlinks=navlinks, current_url=current_url,).encode('utf-8'))
                 return
 
-            self.send_error(404, message="..Template not found")
+            # unknown page, show error
+            self.send_error(404)
             return
-
-        try:
-            template = env.get_template(f'{template_name}.html')
-        except TemplateNotFound:
-            self.send_header('Content-type', 'text/html')
-
-            self.send_error(404, message="Template not found")
-            return
-
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        # if template_name == 'index':
-        #     events = [
-        #
-        #     ]
-
-        self.wfile.write(template.render(navlinks=navlinks, current_url=current_url, events=events).encode('utf-8'))
-
-        self.wfile.write('wow'.encode('utf-8'))
-        return
 
 
 def run(server_class=HTTPServer, handler_class=ExtendedHTTPRequestHandler):
